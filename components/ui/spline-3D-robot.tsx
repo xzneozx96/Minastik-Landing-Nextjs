bfc8530d2c68cb5f1a677c5616666a348d7a15c3
Ross Geller <tuananhngyen218@gmail.com>
2024-11-13 19:08:20 +0700

    UI for projects showcase

--- components/ui/spline-3D-robot.tsx
+++ components/ui/spline-3D-robot.tsx
@@ -2,24 +2,25 @@
 
 import Spline from '@splinetool/react-spline';
 import { useInView } from 'framer-motion';
-import { Suspense, useRef } from 'react';
+import { useRef } from 'react';
 
 export default function SplineRobot() {
 	const splineRef = useRef(null);
-	const inView = useInView(splineRef);
+	const inView = useInView(splineRef, { amount: 0.5 });
+
+	console.log('inView', inView);
 
 	return (
-		<Suspense fallback={<div>Loading...</div>}>
-			<div
-				ref={splineRef}
-				style={{ height: '100vh' }}
-			>
-				{inView ? (
-					<Spline scene='https://prod.spline.design/CnLR0haZQQ6vG-xd/scene.splinecode' />
-				) : (
-					<div>Scroll to view the model</div>
-				)}
-				{/* <video
+		<div
+			ref={splineRef}
+			style={{ height: '100vh' }}
+		>
+			{inView ? (
+				<Spline scene='https://prod.spline.design/CnLR0haZQQ6vG-xd/scene.splinecode' />
+			) : (
+				<div>Scroll to view the model</div>
+			)}
+			{/* <video
 					autoPlay
 					muted
 				>
@@ -29,7 +30,6 @@ export default function SplineRobot() {
 					/>
 					Your browser does not support the video tag.
 				</video> */}
-			</div>
-		</Suspense>
+		</div>
 	);
 }
