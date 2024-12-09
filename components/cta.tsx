35ec33fe2908619eb6193b3034c79d2b4580642c
Ross Geller <tuananhngyen218@gmail.com>
2024-12-09 22:14:16 +0700

    fix bug on cta section

--- components/cta.tsx
+++ components/cta.tsx
@@ -52,7 +52,7 @@ export default function Cta() {
 					</div>
 
 					{/* blur background */}
-					<div className='absolute top-1/2 left-1/2 gradient-blur w-[85%] -translate-x-1/2 h-[85%] -translate-y-1/2 inset-0 blur-[5rem]'></div>
+					<div className='absolute top-1/2 left-1/2 gradient-blur w-[85%] -translate-x-1/2 h-[85%] -translate-y-1/2 inset-0 blur-[5rem] -z-10'></div>
 
 					{/* Always visible video */}
 					<div className='absolute top-0 left-0 right-0 bottom-0 md:z-0 z-[-1]'>
